de64d224c588a40faf7e04c2ee9dff9c991867b0
breno.lima <brenoryan21@hotmail.com>
2025-03-08 15:39:23 -0300

    feat: Ajustando AuthGuard para verificar se o token acabou

--- src/components/AuthGuard.tsx
+++ src/components/AuthGuard.tsx
@@ -1,6 +1,7 @@
 
 import { useEffect, useState } from 'react';
 import { Navigate, useLocation } from 'react-router-dom';
+import { useToast } from "@/hooks/use-toast.ts";
 
 interface AuthGuardProps {
   children: React.ReactNode;
@@ -9,24 +10,47 @@ interface AuthGuardProps {
 const AuthGuard = ({ children }: AuthGuardProps) => {
   const location = useLocation();
   const [isAuthenticated, setIsAuthenticated] = useState<boolean | null>(null);
+  const { toast } = useToast();
 
   useEffect(() => {
-    // Check authentication status
-    const authStatus = localStorage.getItem('isAuthenticated') === 'true';
-    setIsAuthenticated(authStatus);
+    const checkAuth = () => {
+      const token = localStorage.getItem('token');
+      if(!token) {
+        setIsAuthenticated(false);
+        return;
+      }
+
+      try {
+        const payload = JSON.parse(atob(token.split('.')[1]));
+        const isTokenExpired = payload.exp * 1000 < Date.now();
+
+        if(isTokenExpired) {
+          toast({
+            title: "Sessão expirada",
+            description: "Por favor, faça login novamente.",
+          });
+          setIsAuthenticated(false);
+          localStorage.removeItem('token');
+        } else {
+            setIsAuthenticated(true);
+        }
+      } catch (error) {
+        setIsAuthenticated(false);
+        console.error("Erro ao verificar token:", error);
+      }
+    };
+
+    checkAuth();
   }, []);
 
-  // Show nothing while checking authentication
   if (isAuthenticated === null) {
     return null;
   }
 
-  // Redirect to login if not authenticated
   if (!isAuthenticated) {
     return <Navigate to="/login" state={{ from: location }} replace />;
   }
 
-  // Render children if authenticated
   return <>{children}</>;
 };
 
